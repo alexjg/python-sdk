ca3178f165f6469206870f56ab916bcda096b431
Alex <alexjsgood@gmail.com>
2012-11-26 15:30:57 +0000

    Remove redundant assignment from auth testcase

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -7,7 +7,7 @@ class AuthenticationTestCase(unittest.TestCase):
     def test_authentication_sets_auth_token(self):
         with mock.patch('kazoo.client.ApiKeyAuthRequest') as mock_req_class:
             mock_req = mock.Mock()
-            mock_req.execute.return_value = json = {"auth_token": "authorizethis"}
+            mock_req.execute.return_value = {"auth_token": "authorizethis"}
             mock_req_class.return_value = mock_req
             client = Client(api_key="dsfjasbfkasdf")
             client.authenticate()
