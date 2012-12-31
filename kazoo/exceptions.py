135527261255db83b08db17072c289752b7649d3
Alex <alexjsgood@gmail.com>
2012-12-31 18:15:31 +0000

    Make invalid data print field errors

--- kazoo/exceptions.py
+++ kazoo/exceptions.py
@@ -16,3 +16,6 @@ class KazooApiBadDataError(RuntimeError):
     def __init__(self, field_errors):
         super(KazooApiBadDataError, self).__init__("Invalid Data")
         self.field_errors = field_errors
+
+    def __str__(self):
+        return "Invalid data, field errors are: {0}".format(self.field_errors)
